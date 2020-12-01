8a0a2e29e39e458fad10b35f351894ece9cc6b87
Alfinity <its.alfinity@gmail.com>
2020-12-01 22:41:22 +0200

    Added source code link

--- source/components/App/index.jsx
+++ source/components/App/index.jsx
@@ -4,18 +4,25 @@ import NewTaskForm from 'Components/NewTaskForm';
 import SearchTask from 'Components/SearchTask';
 import TodoList from 'Components/TodoList';
 
+import './styles.sass';
+
 export default function App() {
   return (
-    <div className='container mt-4'>
-      <div className='row sticky-top bg-light border rounded-lg py-3 mb-3'>
-        <div className='col-sm'>
-            <NewTaskForm />
-          </div>
-          <div className='col-sm'>
-            <SearchTask />
-          </div>
+    <React.Fragment>
+      <div className='corner-ribbon__wrapper'>
+        <a href='https://github.com/Almost-Infinity/ToDo' target='_blank' rel='noopener noreferrer' className='corner-ribbon__link'>source</a>
       </div>
-      <TodoList />
-    </div>
+      <div className='container mt-4'>
+        <div className='row sticky-top bg-light border rounded-lg py-3 mb-3'>
+          <div className='col-sm'>
+              <NewTaskForm />
+            </div>
+            <div className='col-sm'>
+              <SearchTask />
+            </div>
+        </div>
+        <TodoList />
+      </div>
+    </React.Fragment>
   );
 }
